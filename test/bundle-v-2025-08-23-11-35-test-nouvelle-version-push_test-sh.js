d2e36889562096ac9435b13e568de74984ee8745
Olivier Nachin <olivier.nach@gmail.com>
2025-08-23 11:35:19 +0200

    [TEST] Test nouvelle version push_test.sh (version v-2025-08-23-11-35-test-nouvelle-version-push_test-sh)

---
+++ test/bundle-v-2025-08-23-11-35-test-nouvelle-version-push_test-sh.js
@@ -0,0 +1,23 @@
+/* ps-trainer TEST bundle */
+/* version: v-2025-08-23-11-35-test-nouvelle-version-push_test-sh */
+/* build: 2025-08-23T09:35:19Z */
+'use strict';
+
+// ---- Début: test/src/test.js ----
+(function () {
+  const ascii = `
+   ____  ____      ______            _             
+  |  _ \|  _ \    / / ___|___  _ __ | |_ _ __ ___  
+  | |_) | |_) |  / / |   / _ \| '_ \| __| '__/ _ \ 
+  |  __/|  _ <  / /| |__| (_) | | | | |_| | | (_) |
+  |_|   |_| \_\/_/  \____\___/|_| |_|\__|_|  \___/ 
+                                                   
+  `;
+  // Affichage en console
+  console.log(ascii);
+})();
+
+
+
+// ---- Fin: test/src/test.js ----
+
