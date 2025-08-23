c874cdda08ccea2b65ab0ef49469852b9d521438
Olivier Nachin <olivier.nach@gmail.com>
2025-08-23 12:24:20 +0200

    test: Nouveau test de push_test.sh [v-2025-08-23-12-24-nouveau-test-de-push-test-sh]

--- test/bundle_test.js
+++ test/bundle_test.js
@@ -1,2 +1,2 @@
-/* env:test version:v-2025-08-23-12-21-test-de-nouveau-push-test-sh */
+/* env:test version:v-2025-08-23-12-24-nouveau-test-de-push-test-sh */
 (()=>{console.log("Hello World !");})();
